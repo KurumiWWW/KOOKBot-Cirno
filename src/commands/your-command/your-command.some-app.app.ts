5490ff6f3b7499f3ca133774cc0859ee32f360bf
kurumiwww <2209278137@qq.com>
2022-11-03 18:29:02 +0800

    init

--- src/commands/your-command/your-command.some-app.app.ts
+++ src/commands/your-command/your-command.some-app.app.ts
@@ -1,1 +1,19 @@
 // 请参考echo文件夹内的用法
+import { AppCommand, AppFunc, BaseSession } from 'kbotify';
+class TestKmd extends AppCommand {
+    code = 'test'; // 只是用作标记
+    trigger = 'test'; // 用于触发的文字
+    help = '`帮助文字`'; // 帮助文字
+    intro = 'test intro';
+    func: AppFunc<BaseSession> = async (session) => {
+        // console.log(session) 可以在console里查看更多session提供的相关信息
+        // 这些信息可以帮助你获取当前的信息发送者、当前信息发送者的频道等
+        if (!session.args.length) {
+            return session.reply(this.help);
+        }
+        // return session.quote(`${session.args}`);
+        return session.quote('123');
+    };
+}
+
+export const testKmd = new TestKmd();
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import { bot } from 'init/client';
 import { echoMenu } from './commands/echo/echo.menu';
+import { testKmd } from './commands/your-command/your-command.some-app.app';
 
 bot.messageSource.on('message', (e) => {
     bot.logger.debug(`received:`, e);
     // 如果想要在console里查看收到信息也可以用
-    //console.log(e);
+    console.log(e);
 });
 
 bot.addCommands(echoMenu);
+bot.addCommands(testKmd);
 
 bot.connect();
+console.log('bot connect successful');
 
 bot.logger.debug('system init success');
